29ba5b2bed1619f54d4d516fbf31abba796bdfab
beyonwr <beyonwr@gmail.com>
2026-02-05 21:48:45 +0900

    불필요 callback comment out

--- agents/agent.py
+++ agents/agent.py
@@ -5,8 +5,8 @@ from google.adk.models.lite_llm import LiteLlm
 from google.adk.tools.agent_tool import AgentTool 
 
 from .utils.prompt_utils import get_prompt_yaml
-from .utils.file_utils import save_imgfile_artifact_before_agent_callback
-from .utils.file_utils import remove_non_text_part_from_llmrequest_before_model_callback
+# from .utils.file_utils import save_imgfile_artifact_before_agent_callback
+# from .utils.file_utils import remove_non_text_part_from_llmrequest_before_model_callback
 from .sub_agents import data_search_agent
 
 ROOT_AGENT_PROMPT = get_prompt_yaml(tag="prompt")
@@ -22,8 +22,8 @@ root_agent = Agent(
     instruction=ROOT_AGENT_PROMPT,
 
     sub_agents=[data_search_agent],
-    before_agent_callback = save_imgfile_artifact_before_agent_callback,
-    before_model_callback = remove_non_text_part_from_llmrequest_before_model_callback,
+    # before_agent_callback = save_imgfile_artifact_before_agent_callback,
+    # before_model_callback = remove_non_text_part_from_llmrequest_before_model_callback,
     output_key="result",
     global_instruction=GLOBAL_INSTRUCTION,
 )
