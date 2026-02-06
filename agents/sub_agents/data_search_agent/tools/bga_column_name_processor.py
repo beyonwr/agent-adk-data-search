64bbb4caf5a87f971d62bee80d9fe4f55a69d2ac
beyonwr <beyonwr@gmail.com>
2026-02-06 20:06:59 +0900

    env port 수정에 맞춤

--- agents/sub_agents/data_search_agent/tools/bga_column_name_processor.py
+++ agents/sub_agents/data_search_agent/tools/bga_column_name_processor.py
@@ -6,7 +6,7 @@ import chromadb.config
 import requests
 
 CHROMADB_HOST = os.getenv("CHROMADB_HOST")
-CHROMADB_PORT = os.getenv("CHROMADB_PORT", "8000")
+CHROMADB_PORT = os.getenv("CHROMADB_PORT")
 CHROMADB_COLLECTION_NAME = os.getenv("CHROMADB_COLLECTION_NAME")
 TEXT_EMBEDDING_MODEL_URL = os.getenv("TEXT_EMBEDDING_MODEL_URL")
 TEXT_EMBEDDING_MODEL_NAME = os.getenv("TEXT_EMBEDDING_MODEL_NAME")
@@ -31,7 +31,7 @@ def _get_embedding(text_list: list[str]) -> list[list[float]]:
 def get_sim_search(query_list: list[str], n_results: int=3):
     chroma_client = chromadb.HttpClient(
         host=CHROMADB_HOST,
-        port=int(CHROMADB_PORT),
+        port=CHROMADB_PORT,
         settings=chromadb.config.Settings(allow_reset=True, annoymized_telemetry=False)
     )
 
